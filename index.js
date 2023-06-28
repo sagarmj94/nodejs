003331f0808b64e131a3c497cac5e27f1ed12196
sagar <sagarmj1502@gmail.com>
2023-06-28 16:41:21 +0530

    how read file create file dynamically

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 const dirPath = path.join(__dirname, "files");
-console.log(dirPath);
-for (i = 0; i < 5; i++) {
-  fs.writeFileSync(`${dirPath}/hello${i}.txt`, "a simple text file");
-}
+// for (i = 0; i < 5; i++) {
+//   fs.writeFileSync(`${dirPath}/hello${i}.txt`, "a simple text file");
+// }
+
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((fil) => {
+    console.log(fil);
+  });
+});
