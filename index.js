d22fac0557010923ea47b53a6d3fd7220cbb3a2a
sagar <sagarmj1502@gmail.com>
2023-06-28 17:56:20 +0530

    asyn function

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
-const fs = require("fs");
-const path = require("path");
-const dirPath = path.join(__dirname, "crud");
-const filePath = `${dirPath}/apple.txt`;
-
-// fs.writeFileSync(filePath, "This is a simple txt file");
-
-// how to red file
-
-// fs.readFile(filePath, "utf8", (err, item) => {
-//   console.log(item);
-// });
-
-// how to updated files
-
-// fs.appendFile(filePath, " hahahahah and file name i app.txt", (err) => {
-//   if (err) console.log("file is updated");
-// });
-
-// how to rename file name dynamically
-
-// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
-//   if (err) console.log("file name is updated");
-// });
-
-// how to delete file
-
-fs.unlinkSync(`${dirPath}/fruit.txt`);
+let a = 10;
+let b = 0;
+setTimeout(() => {
+  b = 20;
+  console.log("logic", a + b);
+}, 1000);
+console.log("complete exe...", a + b);
