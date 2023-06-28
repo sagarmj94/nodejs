950e88d0892b1a7c67a65986a180c8a9261f0ac4
sagar <sagarmj1502@gmail.com>
2023-06-28 17:00:14 +0530

    how to read files

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const path = require("path");
 const dirPath = path.join(__dirname, "crud");
 const filePath = `${dirPath}/apple.txt`;
 
-fs.writeFileSync(filePath, "This is a simple txt file");
+// fs.writeFileSync(filePath, "This is a simple txt file");
+
+fs.readFile(filePath, "utf8", (err, item) => {
+  console.log(item);
+});
