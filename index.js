1754601189f4bfc958591ac9206ee476b903fbd6
sagar <sagarmj1502@gmail.com>
2023-06-28 17:06:30 +0530

    how to rename file name dynamically

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const filePath = `${dirPath}/apple.txt`;
 
 // fs.writeFileSync(filePath, "This is a simple txt file");
 
+// how to red file
+
 // fs.readFile(filePath, "utf8", (err, item) => {
 //   console.log(item);
 // });
 
-fs.appendFile(filePath, " hahahahah and file name i app.txt", (err) => {
-  if (err) console.log("file is updated");
+// how to updated files
+
+// fs.appendFile(filePath, " hahahahah and file name i app.txt", (err) => {
+//   if (err) console.log("file is updated");
+// });
+
+// how to rename file name dynamically
+
+fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+  if (err) console.log("file name is updated");
 });
