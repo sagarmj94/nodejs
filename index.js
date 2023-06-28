3f668cc36c8ca67cc58af754018cdbb1fca557e1
sagar <sagarmj1502@gmail.com>
2023-06-28 20:16:20 +0530

    render html element and json

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
 
+let dataa = [
+  {
+    name: "sagar",
+    email: "sagarmj@gmail.com",
+  },
+  {
+    name: "sagar",
+    email: "sagarmj@gmail.com",
+  },
+  {
+    name: "sagar",
+    email: "sagarmj@gmail.com",
+  },
+];
+
 app.get("", (req, res) => {
   console.log("data sent by client", req.query.name);
-  res.send(`Hello ${req.query.name}`);
+  res.send(
+    `Hello ${req.query.name} <input type='text' placeholder="user name"/>
+    <button>Click me</button>
+    <a href="/about">go to about</a>
+    `
+  );
 });
 app.get("/about", (req, res) => {
-  res.send("Hello this is About page");
+  res.send(`<a href="/">go to home</a> <br/> ${JSON.stringify(dataa)}`);
 });
 
 app.listen(5000);
