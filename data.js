b3992c7293b654473c6cdc37fec86ecd3ae8d8e9
sagar <sagarmj1502@gmail.com>
2023-06-28 16:21:35 +0530

    how to add file and remove file

--- data.js
+++
@@ -1,6 +0,0 @@
-const data = [
-  { name: "sagar", email: "sagar@gmail.com" },
-  { name: "sam", email: "sam@gmail.com" },
-  { name: "jiyan", email: "jiyan@gmail.com" },
-];
-module.exports = data;
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const http = require("http");
-const data = require("./data");
+const fs = require("fs");
+const color = require("colors");
+const input = process.argv;
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("invalid o/p".red);
+}
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(data));
-    res.end();
-  })
-  .listen(5000);
+// cmd_add_file:_node index.js add orange.txt "this is a color"
+// cmd_add_file:node index.js remove apple.txt
+//cmd_remove_file: node index.js remove orange.txt
