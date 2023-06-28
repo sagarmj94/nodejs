60f58cfd050774e2269e5bcf453eef6142608414
sagar <sagarmj1502@gmail.com>
2023-06-28 15:07:49 +0530

    server create

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+  { name: "sagar", email: "sagar@gmail.com" },
+  { name: "sam", email: "sam@gmail.com" },
+  { name: "jiyan", email: "jiyan@gmail.com" },
+];
+module.exports = data;
