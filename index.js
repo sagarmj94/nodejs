9870bbf29bb913a6558cf29d0c9dccf81131db0f
sagar <sagarmj1502@gmail.com>
2023-06-28 19:53:54 +0530

    routing params geting

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello this is home page");
+  console.log("data sent by client", req.query.name);
+  res.send(`Hello ${req.query.name}`);
 });
 app.get("/about", (req, res) => {
   res.send("Hello this is About page");
