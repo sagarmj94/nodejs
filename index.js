54aac814c7035adfced5fd09fa40a006fae0d8ed
sagar <sagarmj1502@gmail.com>
2023-07-01 00:31:04 +0530

    page not found created

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+
+app.get("/about", (req, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
+app.get("", (req, res) => {
+  res.sendFile(`${publicPath}/index.html`);
+});
+app.get("/help", (req, res) => {
+  res.sendFile(`${publicPath}/help.html`);
+});
+app.get("*", (req, res) => {
+  res.sendFile(`${publicPath}/404.html`);
+});
 
 app.listen(5000);
