853504bc8350adcb4d5c030274b9e45fe1b1228e
sagar <sagarmj1502@gmail.com>
2023-07-01 00:57:55 +0530

    Dynamic Page with ejs

--- index.js
+++ index.js
@@ -5,15 +5,30 @@ const publicPath = path.join(__dirname, "public");
 
 // app.use(express.static(publicPath));
 
+app.set("view engine", "ejs");
+
 app.get("/about", (req, res) => {
   res.sendFile(`${publicPath}/about.html`);
 });
 app.get("", (req, res) => {
   res.sendFile(`${publicPath}/index.html`);
 });
+
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "sagar JAdhav",
+    email: "sagarmj1502@gmail.com",
+    city: "noida",
+    skils: ["php", "css", "javascript"],
+  };
+
+  res.render("profile", { user });
+});
+
 app.get("/help", (req, res) => {
   res.sendFile(`${publicPath}/help.html`);
 });
+
 app.get("*", (req, res) => {
   res.sendFile(`${publicPath}/404.html`);
 });
