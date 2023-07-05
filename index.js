d903dafa5c5a55c2c38107ac123589594c4e0e8b
sagar <sagarmj1502@gmail.com>
2023-07-05 12:45:53 +0530

    created common middleware instead passing same in each routes

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 const reqFilter = require("./middleware");
+const route = express.Router();
 
 // app.use(reqFilter);
 
+route.use(reqFilter);
 app.get("/", (req, res) => {
   res.send("Welcome to Home page");
 });
-app.get("/user", reqFilter, (req, res) => {
+route.get("/user", (req, res) => {
   res.send("Welcome to User page");
 });
-app.get("/about", (req, res) => {
+route.get("/about", (req, res) => {
   res.send("Welcome to about page");
 });
+app.get("/contact", (req, res) => {
+  res.send("Welcome to contact page");
+});
+app.use("/", route);
 
 app.listen(5000);
