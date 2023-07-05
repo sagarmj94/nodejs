0ce50ecc138620b1138c415faac872c785d1d718
sagar <sagarmj1502@gmail.com>
2023-07-05 12:38:11 +0530

    middleware applyon specific route

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ const reqFilter = (req, res, next) => {
   }
 };
 
-app.use(reqFilter);
+// app.use(reqFilter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Home page");
 });
-app.get("/user", (req, res) => {
+app.get("/user", reqFilter, (req, res) => {
   res.send("Welcome to User page");
+});
+app.get("/about", (req, res) => {
+  res.send("Welcome to about page");
 });
 
 app.listen(5000);
